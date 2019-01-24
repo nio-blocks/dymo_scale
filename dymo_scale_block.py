dc84eff0837fdb5e97a7d1f0a910a64e734bbc94
Tom Young <tyoung@n.io>
2019-01-24 12:08:10 -0700

    initial dymo block

--- dymo_scale_block.py
+++ dymo_scale_block.py
@@ -1,12 +1,75 @@
-from nio.block.base import Block
+import usb.core
+from nio import GeneratorBlock, Signal
 from nio.properties import VersionProperty
+from nio.util.threading import spawn
 
 
-class DymoScale(Block):
+class DymoScale(GeneratorBlock):
 
     version = VersionProperty('0.1.0')
+    manufacturer_id = 0x0922
+    product_id = 0x8003
 
-    def process_signals(self, signals):
-        for signal in signals:
-            pass
-        self.notify_signals(signals)
+    def __init__(self):
+        super().__init__()
+        self.device = None
+        self._kill = False
+        self._address = None
+        self._packet_size = None
+
+    def start(self):
+        super().start()
+        self.device = usb.core.find(self.manufacturer_id, self.product_id)
+        self.logger.debug('Device discovered')
+        self.device.reset()
+        self.logger.debug('Device reset')
+        interface = 0
+        if self.device.is_kernel_driver_attached(interface):
+            self.device.detach_kernel_driver(interface)
+            self.logger.debug('Detached kernel driver')
+        self.device.set_configuration()
+        self.logger.debug('Device Configured')
+        self.device.connect()
+        self.logger.debug('Device Connected!')
+        endpoint = self.device[interface][(0, 0)][0]
+        self._address = endpoint.bEndpointAddress
+        self._packet_size = endpoint.wMaxPacketSize
+        spawn(self._reader)
+
+    def stop(self):
+        self.logger.debug('Stopping reader thread')
+        self._kill = True
+        super().stop()
+
+    def _reader(self):
+        self.logger.debug('Reader thread spawned')
+        while not self._kill:
+            data = self.device.read(self._address, self._packet_size)
+            units, weight = self._parse_weight(data)
+            signal_dict = {
+                'units': units,
+                'weight': weight,
+            }
+            self.notify_signals([Signal(signal_dict)])
+        self.logger.debug('Reader thread completed')
+
+    def _parse_weight(self, data):
+        state = data[1]
+        if state == 5:  # scale value is negative
+            sign = -1
+        else:
+            sign = 1
+        units = data[2]
+        if units == 2:
+            units = 'g'
+        else:
+            units = 'oz'
+        factor = data[3]
+        if factor == 255:  # values are multiplied by 10
+            factor = 10
+        else:
+            factor = 1
+        weight_hi = data[4]
+        weight_lo = data[5]
+        weight = (weight_hi + weight_lo) / factor * sign
+        return units, weight
--- tests/test_dymo_scale_block.py
+++ tests/test_dymo_scale_block.py
@@ -1,19 +1,83 @@
+from array import array
+from threading import Event
+from unittest.mock import Mock, patch
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
+from nio.util.discovery import not_discoverable
 from ..dymo_scale_block import DymoScale
 
 
+class DummyDevice():
+
+    dummy_packet = array('B', [3, 4, 11, 255, 2, 0])
+    reset = Mock()
+    is_kernel_driver_attached = Mock()
+    detach_kernel_driver = Mock()
+    set_configuration = Mock()
+    connect = Mock()
+    read = Mock(return_value=dummy_packet)
+
+    def __init__(self):
+        super().__init__()
+        self.endpoint = DummyEndpoint()
+
+    def __getitem__(self, index):
+        return {(0, 0): [self.endpoint]}
+
+class DummyEndpoint():
+
+    bEndpointAddress = 0x82
+    wMaxPacketSize = 0x08
+
+
+# class DummyReader():
+    # def __init__(self):
+        # self._done = False
+
+    # def __call__(self, address, length):
+        # if self._done:
+            # return
+        # self._done = True
+        # return DummyDevice.dummy_packet
+
+@not_discoverable
+class ReadEvent(DymoScale):
+
+    def __init__(self, event):
+        super().__init__()
+        self._event = event
+
+    def notify_signals(self, signals):
+        super().notify_signals(signals)
+        self._event.set()
+
 class TestDymoScale(NIOBlockTestCase):
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
-        blk = DymoScale()
-        self.configure_block(blk, {})
+    @patch('usb.core')
+    def test_connection(self, mock_usb_core):
+        """When started discover and connect to a scale."""
+        mock_device = DummyDevice()
+        mock_device.is_kernel_driver_attached.return_value = True
+        # mock_device.read.side_effect = DummyReader()
+        mock_usb_core.find.return_value = mock_device
+        e = Event()
+        blk = ReadEvent(e)
+        cfg = {'log_level': 'DEBUG'}
+        self.configure_block(blk, cfg)
         blk.start()
-        blk.process_signals([Signal({"hello": "nio"})])
+        e.wait(1)  # wait up to 1 second for signals from block
         blk.stop()
+        mock_usb_core.find.assert_called_once_with(
+            blk.manufacturer_id, blk.product_id)
+        mock_device.reset.assert_called_once_with()
+        mock_device.is_kernel_driver_attached.assert_called_once_with(0)
+        mock_device.detach_kernel_driver.assert_called_once_with(0)
+        mock_device.set_configuration.assert_called_once_with()
+        mock_device.connect.assert_called_once_with()
+        mock_device.read.assert_called_once_with(
+            DummyEndpoint.bEndpointAddress, DummyEndpoint.wMaxPacketSize)
         self.assert_num_signals_notified(1)
         self.assertDictEqual(
             self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "nio"})
+            {'units': 'oz', 'weight': 0.2})
