b3fb662dfcf8e9620bf32a1a74c6f74f57bd3055
Tom Young <tyoung@n.io>
2019-01-24 14:33:00 -0700

    add _disconnect, throttle read rate

--- dymo_scale_block.py
+++ dymo_scale_block.py
@@ -1,3 +1,5 @@
+from threading import current_thread
+from time import sleep
 import usb.core
 from nio import GeneratorBlock, Signal
 from nio.properties import VersionProperty
@@ -7,56 +9,81 @@ from nio.util.threading import spawn
 class DymoScale(GeneratorBlock):
 
     version = VersionProperty('0.1.0')
+
+    read_interval = 0.5  # seconds between reads
+    reconnect_interval = 10
+
     manufacturer_id = 0x0922
     product_id = 0x8003
 
     def __init__(self):
         super().__init__()
         self.device = None
+        self._reader_thread = None
         self._kill = False
+
         self._address = None
         self._packet_size = None
 
     def start(self):
         super().start()
         spawn(self._connect)
-        spawn(self._reader)
 
     def stop(self):
-        self.logger.debug('Stopping reader thread')
-        self._kill = True
+        self._disconnect()
         super().stop()
 
     def _connect(self):
-        self.logger.debug('Connection thread spawned')
-        self.device = usb.core.find(self.manufacturer_id, self.product_id)
-        self.logger.debug('Device discovered')
-        self.device.reset()
-        self.logger.debug('Device reset')
-        interface = 0
-        if self.device.is_kernel_driver_attached(interface):
-            self.device.detach_kernel_driver(interface)
-            self.logger.debug('Detached kernel driver')
-        self.device.set_configuration()
-        self.logger.debug('Device Configured')
-        self.device.connect()
-        self.logger.debug('Device Connected!')
-        endpoint = self.device[interface][(0, 0)][0]
-        self._address = endpoint.bEndpointAddress
-        self._packet_size = endpoint.wMaxPacketSize
-        self.logger.debug('Connection thread completed')
+        self.logger.debug('Connecting to scale device...')
+        while not self.device:
+            try:
+                self.device = usb.core.find(
+                    self.manufacturer_id, self.product_id)
+                self.logger.debug('Device discovered')
+                self.device.reset()
+                self.logger.debug('Device reset')
+                interface = 0
+                if self.device.is_kernel_driver_attached(interface):
+                    self.device.detach_kernel_driver(interface)
+                    self.logger.debug('Detached kernel driver')
+                self.device.set_configuration()
+                self.logger.debug('Device Configured')
+                self.device.connect()
+                self.logger.debug('Device Connected!')
+                endpoint = self.device[interface][(0, 0)][0]
+                self._address = endpoint.bEndpointAddress
+                self._packet_size = endpoint.wMaxPacketSize
+            except:
+                msg = 'Unable to connect to scale, trying again in {} seconds'
+                self.logger.error(msg.format(self.reconnect_interval))
+                sleep(self.reconnect_interval)
+        self._kill = False
+        self._reader_thread = spawn(self._reader)
+
+    def _disconnect(self):
+        self.logger.debug('Halting read operations')
+        self._kill = True
+        self.device = None
 
     def _reader(self):
-        self.logger.debug('Reader thread spawned')
+        thread_id = current_thread().name
+        self.logger.debug('Reader thread {} spawned'.format(thread_id))
         while not self._kill:
-            data = self.device.read(self._address, self._packet_size)
+            try:
+                data = self.device.read(self._address, self._packet_size)
+            except:
+                self.logger.exception('Read operation from scale failed')
+                self._disconnect()
+                self._connect()
+                break
             units, weight = self._parse_weight(data)
             signal_dict = {
                 'units': units,
                 'weight': weight,
             }
             self.notify_signals([Signal(signal_dict)])
-        self.logger.debug('Reader thread completed')
+            sleep(self.read_interval)
+        self.logger.debug('Reader thread {} completed'.format(thread_id))
 
     def _parse_weight(self, data):
         # battery = data[0]
